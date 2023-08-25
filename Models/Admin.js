65ebcddbd9791be0a0e92251ed1ae84136a84403
alucard2023 <hadded.tarek@gmail.com>
2023-08-25 20:07:36 +0100

    pp

---
+++ Models/Admin.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const {Schema , model} = mongoose ;
+
+const AdminSchema = mongoose.Schema({
+
+    firstname : {
+        type : String,
+        required : true
+    },
+
+    lastname : {
+        type : String,
+        required : true
+    },
+
+    email : {
+        type : String,
+        required : true ,
+        unique : true 
+    },
+    password : {
+        type : String,
+        required : true ,
+    },
+    isAdmin:{ type: Boolean},
+
+
+})
+
+
+module.exports = Admin = mongoose.model('Admin',AdminSchema)
